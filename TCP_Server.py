8038882b6639024b4c441b1ce7e0012f8f601b57
Quinn Bigane <51971293+QuinnBigane@users.noreply.github.com>
2022-04-21 10:32:18 -0700

    Added stamping of ID/timestamps, implemented parsing for reliability and latency

--- TCP_Server.py
+++ TCP_Server.py
@@ -1,20 +1,70 @@
-import socket
+"""
+Title: TCP_Server.py
+
+Authors:
+Quinn Bigane 
+
+Date
+4/21/22
+
+This File will implement a server in a TCP connection.
+It will recieve json messages and, if it is in test mode,
+it will parse those messages and determine the reliability 
+percentage and average latency of the connection.
+"""
+
+#TODO: Parse all the recieved messages for reliability percentages based on ID
+#TODO: Parse all the recieved messages for a latency based on timestamps
+
+
+import socket 
+import time
 
 def Main():
-    host = '192.168.1.120'
+    #Source IP (rotuer LAN IP)
+    #host = '192.168.1.120'
+    host = '172.17.7.175'
+    #Source Port
     port = 50002
+
+    #Create and conenct to the socket
     s = socket.socket()
     s.bind((host, port))
-
     s.listen(1)
     c, addr = s.accept()
+
+    #create a list to hold info
+    messages = {}
     while True:
+        #receive the data
         print("Waiting To Recv")
-        data = c.recv(1024)
+        data = c.recv(3072)
         if not data:
             break
+
+        #add a timestamp of when we received
+        recv_time = str(time.time())
         data = str(data).upper()
-        print(data)
+        final = f"recieved: {recv_time} {data}"
+        #parse that info
+        toks = final.split()
+        #tok 1 is recv'd time, tok 3 is ID, tok 5 is sent time
+        messages[toks[3]] = f"{toks[1]} {toks[5]}"
+    
+    
+    messages_expected = 10
+    messages_recv = 0
+    avg_latency = 0
+    for i in range(messages_expected):
+        key = str(i)
+        if key in messages.keys():
+            messages_recv +=1
+            message_toks = messages[key].split()
+            avg_latency += float(message_toks[0]) - float(message_toks[1])
+    avg_latency /= messages_recv
+    reliability = messages_recv/messages_expected
+    print(f"Reliability: {reliability * 100}")
+    print(f"Average Latency: {avg_latency}")
     c.close()
 if __name__ == '__main__':
     Main()
--- Test.py
+++ Test.py
@@ -1,32 +1,12 @@
-"""
-Title: TCP_Client.py
-
-Authors:
-Quinn Bigane 
-
-Date
-4/21/22
-
-This File will implement a client in a TCP connection.
-It will send 1 json messages 2kb in size once a second 
-for 100 seconds. 
-"""
-
-import socket
 import time
 
-def Main():
-    #Destination IP (Router WAN IP)
-    #host = '76.218.254.93' 
-    host = '172.17.7.175'
-    #Destination port (Port open on router)
-    port = 50002 
-    
-    #Create and connect to the socket
-    s = socket.socket()
-    s.connect((host,port))
-    
-    buff_string = str({
+
+msgs_sent = 0
+while msgs_sent < 100 :
+    #add timestamp to string
+    string = "" 
+    string = f"ID: {msgs_sent} time: {time.time()}" 
+    string += str({
     "name": "Molecule Man",
     "age": 29,
     "secretIdentity": "Dan Jukes",
@@ -130,24 +110,7 @@ def Main():
       "BUFFERBUFFERBUFFER",
       "BUFFERBUFFERBUFFER",
       "BUFFERBUFFERBUFFER"
-    ]
-  })
+    print(string)
+    time.sleep(1)
+    msgs_sent += 1
 
-    msgs_sent = 0
-    while msgs_sent < 10 :
-        if msgs_sent == 7:
-            msgs_sent+=1
-            continue
-        #Add ID and timestamp to message
-        string = "" 
-        string = f"ID: {msgs_sent} sent: {time.time()} " 
-        string += buff_string
-        message = bytes(string, 'ascii')
-        s.send(message)
-        print("Sent")
-        time.sleep(1)
-        msgs_sent+=1
-    s.close()
-
-if __name__ == '__main__':
-    Main()
